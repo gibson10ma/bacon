fc5159e910a8d68a8769bdd5694c109bd90c603e
Andrew Nalband <gibson10ma@yahoo.com>
2012-02-06 13:24:40 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bacon::Application.config.secret_token = 'd7d49a7749423c3895245121c199de3a595faef661cbfaf16e336d00c51f18bde16edb0a6ae5804f11473fb6da6d6e81f71e6c0d99faea151b46d0978e5becaf'
